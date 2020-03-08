ef6132f9ee78a630d6697d6b7d9f3095f3165c61
alperkurtul <alperkurtul@gmail.com>
2020-03-08 22:28:09 +0300

    commit and push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Button from './components/UI/Button/Button';
+import WeatherInfo from "./components/Weather/WeatherInfo/WeatherInfo";
+
+class App extends React.Component {
+
+    state = {
+        getData : null,
+        weatherDataLoaded : 'ZZZZZ'
+    }
+
+    getData = () => {
+        this.setState({getData: true});
+    }
+
+    clearData = () => {
+        this.setState({getData: false});
+    }
+
+    render() {
+
+        let weather = this.state.getData ? <WeatherInfo weatherData={this.state.weatherDataLoaded}></WeatherInfo> : '';
+
+        return (
+            <div className="App">
+            {/*<header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            </header>*/}
+                <div><Button clicked={this.getData}>İSTANBUL Hava Durumu</Button></div>
+                <div><Button clicked={this.clearData}>Temizle</Button></div>
+                {weather}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Weather/WeatherInfo/WeatherInfo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const weatherInfo = (props) => (
+    <div>{props.weatherData}</div>
+);
+
+export default weatherInfo;
