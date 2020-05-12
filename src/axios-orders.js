abe9e3736ae6a4f5dace2a4f25c0de2ef829e060
alperkurtul <alperkurtul@gmail.com>
2020-05-12 19:09:18 +0300

    commit and push

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    //baseURL: `${process.env.REACT_APP_FB_ROOT_API_URL}`
-    baseURL: 'http://localhost:8080'
+    baseURL: `${process.env.REACT_APP_WEATHER_ME_SPRING_BOOT_API_URL}`
 });
 
 export default instance;
